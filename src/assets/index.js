32f98980148f9e9019b58280a5b1d6bfcf131d2c
AndreasPilgerstorfer <71932374+AndreasPilgerstorfer@users.noreply.github.com>
2023-12-29 16:45:13 +0100

    [3] Exchange Logo

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,4 +1,4 @@
-import logo from "./logo/logo-white.svg";
+import logo from "./logo/logo-no-background.svg";
 import backend from "./backend.png";
 import creator from "./creator.png";
 import mobile from "./mobile.png";
