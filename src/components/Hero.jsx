465bc1d8c6daf4eecd5c5d0d17e2cc716cb2f822
AndreasPilgerstorfer <71932374+AndreasPilgerstorfer@users.noreply.github.com>
2023-12-29 17:07:25 +0100

    [4] Remove Computer Canvas

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,5 @@
 import { motion } from "framer-motion";
-
 import { styles } from "../styles";
-import { ComputersCanvas } from "./canvas";
 
 const Hero = () => {
   return (
@@ -16,16 +14,15 @@ const Hero = () => {
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className='text-[#915EFF]'>Adrian</span>
+            Hi, I'm <span className='text-[#915EFF]'>Andreas</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I develop 3D visuals, user <br className='sm:block hidden' />
-            interfaces and web applications
+            Todo <br className='sm:block hidden' />
+            Todo
           </p>
         </div>
       </div>
 
-      <ComputersCanvas />
 
       <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
         <a href='#about'>
