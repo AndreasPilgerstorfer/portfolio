11dba631d6a67ffb45676ddfdf6c25daf2546534
AndreasPilgerstorfer <71932374+AndreasPilgerstorfer@users.noreply.github.com>
2023-12-30 11:50:47 +0100

    [13] Fix anchor link of scroll indicator

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -25,7 +25,7 @@ const Hero = () => {
 
 
       <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
-        <a href='#about'>
+        <a href='#introduction'>
           <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
             <motion.div
               animate={{
