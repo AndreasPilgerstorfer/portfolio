4d796eef0811ffcd19070670ef305596bb8737dc
AndreasPilgerstorfer <71932374+AndreasPilgerstorfer@users.noreply.github.com>
2024-01-10 11:20:19 +0100

    [DEV] Add Motion to About.jsx

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -27,7 +27,9 @@ const About = () => {
                 variants={fadeIn("", "", 0.1, 1)}
                 className='mt-8 grid grid-cols-1 md:grid-cols-12 gap-4'
             >
-                <div className="rounded-lg md:col-span-5 about__image-container">
+                <motion.div
+                    variants={fadeIn("", "", 0.1, 1)}
+                    className="rounded-lg md:col-span-5 about__image-container">
                     <div className="about__image about__image--running p-8">
                         <p><strong>Running</strong></p>
                         <br/>
@@ -38,8 +40,10 @@ const About = () => {
                             </li>
                         </ul>
                     </div>
-                </div>
-                <div className="rounded-lg md:col-span-7 about__image-container">
+                </motion.div>
+                <motion.div
+                    variants={fadeIn("", "", 0.1, 1)}
+                    className="rounded-lg md:col-span-7 about__image-container">
                     <div className="about__image about__image--plane p-8">
                         <p><strong>Travelling the world</strong></p>
                         <br/>
@@ -50,45 +54,57 @@ const About = () => {
                             </li>
                         </ul>
                     </div>
-                </div>
+                </motion.div>
 
 
-                <div className="bg-black-100 rounded-lg p-4 md:col-span-4">
+                <motion.div
+                    variants={fadeIn("", "", 0.1, 1)}
+                    className="bg-black-100 rounded-lg p-4 md:col-span-4">
                     <p><strong>Tennis</strong></p> <br/>
                     <span>
                         Playing tennis with friends brings me sheer joy and camaraderie, blending the thrill of the game with cherished moments of togetherness.
                     </span>
-                </div>
-                <div className="rounded-lg md:col-span-4 about__image-container">
+                </motion.div>
+                <motion.div
+                    variants={fadeIn("", "", 0.1, 1)}
+                    className="rounded-lg md:col-span-4 about__image-container">
                     <div className="about__image about__image--small about__image--table-tennis p-5">
                         <p><strong>Table tennis</strong></p> <br/>
                         <br/>
                           As a competitive table tennis player for the past 13 years, I've honed my skills and competed in championships, clinching victories in several tournaments along the way.
                     </div>
-                </div>
-                <div className="bg-black-100 rounded-lg p-4 md:col-span-4">
+                </motion.div>
+                <motion.div
+                    variants={fadeIn("", "", 0.1, 1)}
+                    className="bg-black-100 rounded-lg p-4 md:col-span-4">
                     <p><strong>Cycling</strong></p> <br/>
                     <span>
                         Cycling's serene and rhythmic nature brings me immense calm and peace, making it a cherished activity.
                     </span>
-                </div>
+                </motion.div>
 
 
-                <a href="https://www.linkedin.com/in/andreas-pilgerstorfer-60656b186/"
+                <motion.a
+                    variants={fadeIn("", "", 0.1, 1)}
+                    href="https://www.linkedin.com/in/andreas-pilgerstorfer-60656b186/"
                    target="_blank" rel="noopener"
                    className="bg-black-100 hover:bg-blue-900 rounded-lg p-4 md:col-span-6 flex justify-center items-center">
                     <img src={linkedin} alt="Linkedin" className="h-10 mr-4"/>
                     <span>Linkedin</span>
-                </a>
-                <a href="https://github.com/AndreasPilgerstorfer"
+                </motion.a>
+                <motion.a
+                    variants={fadeIn("", "", 0.1, 1)}
+                    href="https://github.com/AndreasPilgerstorfer"
                    target="_blank" rel="noopener"
                    className="bg-black-100 hover:bg-gray-900 rounded-lg p-4 md:col-span-6 flex justify-center items-center">
                     <img src={github} alt="Github" className="h-10 mr-4"/>
                     <span>Github</span>
-                </a>
+                </motion.a>
 
 
-                <a href="https://www.youtube.com/watch?v=vqFaMSXxhmo"
+                <motion.a
+                    variants={fadeIn("", "", 0.1, 1)}
+                    href="https://www.youtube.com/watch?v=vqFaMSXxhmo"
                    className="rounded-lg md:col-span-6 about__image-container"
                    target="_blank" rel="noopener">
                     <div className="about__image about__image--design p-8 ">
@@ -105,9 +121,11 @@ const About = () => {
                             </li>
                         </ul>
                     </div>
-                </a>
+                </motion.a>
 
-                <a href="https://tractive.com/blog/en/life-at-tractive/tractive-honeypots"
+                <motion.a
+                   variants={fadeIn("", "", 0.1, 1)}
+                   href="https://tractive.com/blog/en/life-at-tractive/tractive-honeypots"
                    className="rounded-lg md:col-span-6 about__image-container"
                    target="_blank" rel="noopener">
                     <div className="about__image about__image--cyber p-8">
@@ -120,16 +138,17 @@ const About = () => {
                             </li>
                         </ul>
                     </div>
-                </a>
+                </motion.a>
 
-
-                <a href="http://www.kollektiv-salzstrasse.at/" target="_blank" rel="noopener"
+                <motion.a
+                    variants={fadeIn("", "", 0.1, 1)}
+                    href="http://www.kollektiv-salzstrasse.at/" target="_blank" rel="noopener"
                    className="bg-black-100 rounded-lg p-4 md:col-span-12 hover:bg-pink-900">
                     <div>
                         <strong>Kollektiv Salzstraße: &nbsp;</strong>
                         <span>Created a website for two aspiring artists in order to help them to promote their work.</span>
                     </div>
-                </a>
+                </motion.a>
             </motion.div>
         </div>
     );
